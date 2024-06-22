b92554154aa676ca423e1efebb527aec65c75b38
jayden-jy <markup.jy@gmail.com>
2024-06-22 11:47:20 +0900

    docs: 동적스타일링

--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -1,9 +1,9 @@
 // children은 리액트 리능 고유명
 // onSelect는 임의로 이름변경가능 앞에 on을 붙이는게 자주쓰이는 규칙중에 하나
 
-export default function TabButton({ children, onSelect }) {
+export default function TabButton({ children, onSelect, isSelected }) {
   return (
-    <li><button onClick={onSelect}>{children}</button></li>
+    <li><button className={isSelected ? 'active' : undefined} onClick={onSelect}>{children}</button></li>
   );
 }
 
